b378f480bb75ab4544b1f302cf4db45df7cd1e02
Kelly Zhou <yinglinzhou0@gmail.com>
2023-02-08 11:42:08 -0500

    creating tables

--- import_db.sql
+++ import_db.sql
@@ -14,3 +14,34 @@ CREATE TABLE questions(
 
     FOREIGN KEY(associated_author_id) REFERENCES users(id)
 );
+
+CREATE TABLE question_follows(
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE replies(
+    id INTEGER PRIMARY KEY, 
+    subject_question_id INTEGER NOT NULL,
+    parent_reply_id INTEGER, 
+    body TEXT NOT NULL,
+    associated_author_id INTEGER NOT NULL,
+
+    FOREIGN KEY (subject_question_id) REFERENCES questions(id)
+    FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
+    FOREIGN KEY (associated_author_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_likes(
+    id INTEGER PRIMARY KEY, 
+    user_like BOOLEAN NOT NULL,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL
+
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
